72108f1b9ae8c631c56c5217c438d8131462f93c
ViliG <viliana.georgieva83@gmail.com>
2025-11-17 10:55:04 +0100

    limited courses display to only the first three

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -2,12 +2,14 @@ import courses from "../courses.json";
 import CourseCard from "./CourseCard";
 
 const CourseList = () => {
+  const limitedCourses = courses.slice(0, 3);
+
   return (
     <section className="px-4 py-10">
       <h2 className="text-3xl font-bold mb-6 text-center">Available courses</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {courses.map((course) => (
+        {limitedCourses.map((course) => (
           <CourseCard key={course.id} course={course} />
         ))}
       </div>
