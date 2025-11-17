a0614c952f2ced0085c4602b272527828bcbccf1
ViliG <viliana.georgieva83@gmail.com>
2025-11-17 14:49:00 +0100

    tailwindcss additins and adaptations

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,6 +1,6 @@
 const CourseCard = ({ course }) => {
   return (
-    <div className="bg-white rounded-xl shadow p-4">
+    <div className="bg-gray-100 rounded-xl shadow p-4">
       <h3 className="text-xl font-bold mb-2">{course.title}</h3>
       <p className="text-gray-500 mb-1">Level: {course.level}</p>
       <p className="text-gray-500 mb-1">Duration: {course.duration}</p>
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -5,8 +5,8 @@ const CourseList = () => {
   const limitedCourses = courses.slice(0, 3);
 
   return (
-    <section className="px-4 py-10">
-      <h2 className="text-3xl font-bold mb-6 text-center">Available courses</h2>
+    <section className="container m-auto px-10">
+      <h2 className="text-3xl text-center ">Available courses</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {limitedCourses.map((course) => (
